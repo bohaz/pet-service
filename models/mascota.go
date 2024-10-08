ede83318323f63545932851d0d00a667a8f8b7e9
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-10-08 10:41:20 -0300

    Add a db for testing

--- models/mascota.go
+++ models/mascota.go
@@ -1,6 +1,8 @@
 package models
 
 import (
+	"flag"
+
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -16,15 +18,25 @@ type Mascota struct {
 
 // ConnectDatabase conecta a la base de datos y realiza la migración del modelo.
 func ConnectDatabase() error {
-	dsn := "host=localhost user=postgres password=samba1702 dbname=pets_db port=5432 sslmode=disable"
+	var dsn string
+	if isTestMode() {
+			dsn = "host=localhost user=postgres password=samba1702 dbname=pets_db_test port=5432 sslmode=disable"
+	} else {
+			dsn = "host=localhost user=postgres password=samba1702 dbname=pets_db port=5432 sslmode=disable"
+	}
+
 	var err error
 	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
-		return err
+			return err
 	}
 
-	// Migrar el esquema
 	return db.AutoMigrate(&Mascota{})
+}
+
+// Verifica si se está ejecutando en modo de pruebas
+func isTestMode() bool {
+	return flag.Lookup("test.v") != nil
 }
 
 // GetDB devuelve la instancia de la base de datos
